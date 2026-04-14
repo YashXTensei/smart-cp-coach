9130e565ce0cc5f37e6c5291a72a6e80826b03fc
YashXTensei <yashmittal30062007.in>
2026-04-14 23:51:04 +0530

    Major changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=2#ropbl**lt2h==uwv220e#mw%n@s56rf7c+msww*&wo-xpag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
